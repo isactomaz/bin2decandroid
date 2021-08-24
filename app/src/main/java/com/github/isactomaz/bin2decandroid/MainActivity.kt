0b494d5b1d4a9c348c280e8470bacf79f7305ba6
Isac Tomaz da Silva <isactomaz@protonmail.com>
2021-08-24 17:59:29 -0300

    Show result if valid

--- app/src/main/java/com/github/isactomaz/bin2decandroid/MainActivity.kt
+++ app/src/main/java/com/github/isactomaz/bin2decandroid/MainActivity.kt
@@ -9,9 +9,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -36,36 +34,42 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun HomeScreen() {
-    val number = remember { mutableStateOf("") }
-    val isError = remember { mutableStateOf(false) }
-    val placeholder = remember { mutableStateOf("") }
+    var number by remember { mutableStateOf("") }
+    var binary by remember { mutableStateOf("") }
+    var isError by remember { mutableStateOf(false) }
+    var placeholder by remember { mutableStateOf("") }
     Column(
         modifier = Modifier
             .padding(8.dp)
             .fillMaxWidth(),
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
+        if (binary.isNotEmpty()) {
+            Spacer(modifier = Modifier.padding(8.dp))
+            Text(text = "Result = $binary")
+            Spacer(modifier = Modifier.padding(8.dp))
+        }
         OutlinedTextField(
-            value = number.value,
-            onValueChange = { number.value = it },
+            value = number,
+            onValueChange = { number = it },
             label = { Text(text = stringResource(R.string.input_number)) },
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-            isError = isError.value,
-            placeholder = { Text(placeholder.value) },
+            isError = isError,
+            placeholder = { Text(placeholder) },
         )
         Spacer(modifier = Modifier.padding(8.dp))
         Button(
             onClick = {
-                val result = convertNumber(number.value)
+                val result = convertNumber(number)
                 if (result.second) {
-                    isError.value = result.second
-                    number.value = ""
-                    placeholder.value = result.first
+                    number = ""
+                    binary = ""
+                    placeholder = result.first
                 } else {
-                    isError.value = false
-                    number.value = result.first
-                    placeholder.value = ""
+                    binary = result.first
+                    placeholder = ""
                 }
+                isError = result.second
             }
         ) {
             Text(text = stringResource(R.string.convert))
